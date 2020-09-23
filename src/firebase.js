6616c55d52a4e17040b89c5c59525f517c095273
Ryan Kim <iawal86@gmail.com>
2020-09-22 22:34:49 -0700

    created firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseApp = {
+  apiKey: "AIzaSyBAWBgqSGmNe0k_5TBPWHhr_FakliEcdYc",
+  authDomain: "instagram-clone-cf94f.firebaseapp.com",
+  databaseURL: "https://instagram-clone-cf94f.firebaseio.com",
+  projectId: "instagram-clone-cf94f",
+  storageBucket: "instagram-clone-cf94f.appspot.com",
+  messagingSenderId: "592077016926",
+  appId: "1:592077016926:web:8aa83f14a86e536f4809cf",
+};
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebaseApp.storage();
+
+export { db, auth, storage };
