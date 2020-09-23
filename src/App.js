feba2d7bd6baadf3626d906c88225a3f79d20a38
Ryan Kim <iawal86@gmail.com>
2020-09-22 21:56:15 -0700

    created header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 // React
 import React from "react";
 
+// Components
+import Post from "./components/Post";
+
 // CSS
 import "./App.css";
 
 function App() {
-  return <div className="app">Hi</div>;
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      <Post />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,15 @@
+// React
+import React from "react";
+
+// CSS
+import "./Post.css";
+
+function Post() {
+  return (
+    <>
+      <div className="post"></div>
+    </>
+  );
+}
+
+export default Post;
